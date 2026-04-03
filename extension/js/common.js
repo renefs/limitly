ce884f93d81a6338ffac6f378ecdc95a74e9fc43
Rene Fernandez <rene.fernandez@cern.ch>
2026-04-03 09:16:15 +0200

    Refactor message handling and add remaining time calculation functions

--- extension/js/common.js
+++ extension/js/common.js
@@ -32,3 +32,34 @@ const StorageAPI = {
   set: (items) => new Promise(resolve => chrome.storage.local.set(items, resolve)),
   remove: (keys) => new Promise(resolve => chrome.storage.local.remove(keys, resolve))
 };
+
+// Calculate remaining time from a limit (in minutes) and time spent (in ms)
+function getRemainingTime(limitMinutes, spentMs) {
+  const limitMs = (limitMinutes || 0) * 60 * 1000;
+  const remainMs = Math.max(0, limitMs - spentMs);
+  const remainMin = Math.ceil(remainMs / 60000);
+  return { limitMs, remainMs, remainMin };
+}
+
+// Message type constants
+const MSG = {
+  CONFIG_UPDATED:    'CONFIG_UPDATED',
+  RENAME_CATEGORY:   'RENAME_CATEGORY',
+  REMOVE_TARGET:     'REMOVE_TARGET',
+  RESET_TRACKING:    'RESET_TRACKING',
+  GET_TRACKING_DATA: 'GET_TRACKING_DATA',
+  GET_CURRENT_STATE: 'GET_CURRENT_STATE',
+  OPEN_OPTIONS_PAGE: 'OPEN_OPTIONS_PAGE',
+  TIMER_UPDATE:      'TIMER_UPDATE',
+  DAY_ROLLED_OVER:   'DAY_ROLLED_OVER',
+};
+
+// Promise-based message sender
+function sendMessage(type, data = {}) {
+  return new Promise((resolve) => {
+    chrome.runtime.sendMessage({ type, ...data }, (response) => {
+      if (chrome.runtime.lastError) resolve(undefined);
+      else resolve(response);
+    });
+  });
+}
